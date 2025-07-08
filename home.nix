e018a4bf45f8e6c7d21ab9fc9a6a6ac56f31f5ec
Chillzontoast <ibinujaleel6@gmail.com>
2025-07-08 20:17:14 +0000

    add nh and gruvbox kitty theme

--- home.nix
+++ home.nix
@@ -20,9 +20,17 @@
 			la = "ls -A";
 			".." = "cd ..";
 		};
-		initContent = "
+		initContent = ''
+			RED='%F{167}'      # Gruvbox red
+    			YELLOW='%F{214}'   # Gruvbox yellow
+			GREEN='%F{142}'    # Gruvbox green
+			BLUE='%F{109}'     # Gruvbox blue
+			PURPLE='%F{175}'   # Gruvbox purple
+		        RESET='%f'
+			
+			PROMPT="''${RED}[''${YELLOW}%n''${GREEN}@''${BLUE}%m''${RED}:''${PURPLE}%~''${RED}]''${RESET}$ "
 			neofetch
-		";
+		'';
 	};
 
 	programs.kitty = {
@@ -31,9 +39,9 @@
 			size = 10;
 			name = "JetBrains Mono";
 		};
-
+		themeFile = "gruvbox-dark";
 		settings = {
-			background_opacity = "0.9";
+			background_opacity = "1";
 		};
 	};
 	
@@ -45,6 +53,13 @@
 		extraConfig = {
 			init.defaultBranch = "main";
 		};
+	};
+	
+	programs.nh = {
+		enable = true;
+		clean.enable = true;
+		clean.extraArgs = "--keep-since 4d --keep 3";
+		flake = "/home/ibilees/nixos-config";
 	};
 
 	home.sessionVariables = {
